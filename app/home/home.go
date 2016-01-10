f0f996626605bd5ed565867757daa129685cce1d
grunmax <grun.max@gmail.com>
2016-01-10 21:50:32 +0200

    init commit

--- app/home/home.go
+++ app/home/home.go
@@ -1,22 +1,22 @@
 package home
 
 import (
-    "net/http"
-    "html/template"
+	"html/template"
+	"net/http"
 
-    "git-go-websiteskeleton/app/common"
+	"github.com/grunmax/git-go-websiteskeleton/app/common"
 )
 
 func GetHomePage(rw http.ResponseWriter, req *http.Request) {
-    type Page struct {
-        Title string
-    }
-    
-    p := Page{
-        Title: "home",
-    }
+	type Page struct {
+		Title string
+	}
 
-    common.Templates = template.Must(template.ParseFiles("templates/home/home.html", common.LayoutPath))
-    err := common.Templates.ExecuteTemplate(rw, "base", p)
-    common.CheckError(err, 2)
+	p := Page{
+		Title: "home",
+	}
+
+	common.Templates = template.Must(template.ParseFiles("templates/home/home.html", common.LayoutPath))
+	err := common.Templates.ExecuteTemplate(rw, "base", p)
+	common.CheckError(err, 2)
 }
--- app/user/home.go
+++ app/user/home.go
@@ -1,22 +1,22 @@
 package user
 
 import (
-    "net/http"
-    "html/template"
-    
-    "git-go-websiteskeleton/app/common"
+	"html/template"
+	"net/http"
+
+	"github.com/grunmax/git-go-websiteskeleton/app/common"
 )
 
 func GetHomePage(rw http.ResponseWriter, req *http.Request) {
-    type Page struct {
-        Title string
-    }
+	type Page struct {
+		Title string
+	}
 
-    p := Page{
-        Title: "user_home",
-    }
+	p := Page{
+		Title: "user_home",
+	}
 
-    common.Templates = template.Must(template.ParseFiles("templates/user/home.html", common.LayoutPath))
-    err := common.Templates.ExecuteTemplate(rw, "base", p)
-    common.CheckError(err, 2)
+	common.Templates = template.Must(template.ParseFiles("templates/user/home.html", common.LayoutPath))
+	err := common.Templates.ExecuteTemplate(rw, "base", p)
+	common.CheckError(err, 2)
 }
